fae937e25b9ce45ba6904cc7d9c1bcd71b51872d
Cameron Fairchild <cameron@fairchild.software>
2024-09-19 14:45:55 -0400

    capitalize config vars

--- main.py
+++ main.py
@@ -13,31 +13,32 @@ target = "5DS8yTBRZJ8DRCkosBss2ELfSDWBy2ZkWmjMLYciswZX3Rad"
 target_as_bytes = substrateinterface.keypair.Keypair(ss58_address=target).public_key
 target_as_bytes, len(target_as_bytes)
 
-batch_size = 100
+BATCH_SIZE = 100
 outer_batch = 100
+MAX_WORKERS = 64*2
 
-start = 0 * batch_size
+START = 0 * BATCH_SIZE
 
 num_batches = math.factorial(len(first))
 
-def run_batch(start: int, batch_size: int) -> Optional[str]:
-    result = bt_crack.crack([], first, target_as_bytes, start, batch_size)
+def run_batch(START: int, BATCH_SIZE: int) -> Optional[str]:
+    result = bt_crack.crack([], first, target_as_bytes, START, BATCH_SIZE)
     if result is not None:
         return result
     
     return None
 
 def run_(x):
-    return run_batch(x, batch_size)
+    return run_batch(x, BATCH_SIZE)
 
 def main():
-    batches = iter(range(start, num_batches, batch_size))
-    len_batches = (num_batches - start) / batch_size
-    outer_batches = math.ceil((num_batches - start) / (outer_batch * batch_size))
+    batches = iter(range(START, num_batches, BATCH_SIZE))
+    len_batches = (num_batches - START) / BATCH_SIZE
+    outer_batches = math.ceil((num_batches - START) / (outer_batch * BATCH_SIZE))
 
     
     with tqdm(total=len_batches) as pbar:
-        with concurrent.futures.ProcessPoolExecutor(max_workers=64*2) as executor:    
+        with concurrent.futures.ProcessPoolExecutor(max_workers=MAX_WORKERS) as executor:    
             for _ in range(outer_batches):
                 this_batch = [next(batches) for _ in range(outer_batch)]
                 futures = {executor.submit(run_, batch): batch for batch in this_batch}
