c0b538289912b4e6e0a781ba5e1f5f8e7c1fb709
Cameron Fairchild <cameron@fairchild.software>
2024-09-19 14:46:18 -0400

    fmt

--- bt_crack.pyi
+++ bt_crack.pyi
@@ -1,7 +1,15 @@
 from typing import List, Optional
 
-def crack(dictionary: List[str], mnemonic: List[str], target: bytes, start: int, batch_size: int) -> Optional[str]:
+def crack(
+    dictionary: List[str],
+    mnemonic: List[str],
+    target: bytes,
+    start: int,
+    batch_size: int,
+) -> Optional[str]:
     pass
 
-def try_pair_permutations(mnemonic: List[str], target: bytes, start: int, batch_size: int) -> Optional[str]:
+def try_pair_permutations(
+    mnemonic: List[str], target: bytes, start: int, batch_size: int
+) -> Optional[str]:
     pass
--- main.py
+++ main.py
@@ -15,30 +15,34 @@ target_as_bytes, len(target_as_bytes)
 
 BATCH_SIZE = 100
 outer_batch = 100
-MAX_WORKERS = 64*2
+MAX_WORKERS = 64 * 2
 
 START = 0 * BATCH_SIZE
 
 num_batches = math.factorial(len(first))
 
+
 def run_batch(START: int, BATCH_SIZE: int) -> Optional[str]:
     result = bt_crack.crack([], first, target_as_bytes, START, BATCH_SIZE)
     if result is not None:
         return result
-    
+
     return None
+
 
 def run_(x):
     return run_batch(x, BATCH_SIZE)
+
 
 def main():
     batches = iter(range(START, num_batches, BATCH_SIZE))
     len_batches = (num_batches - START) / BATCH_SIZE
     outer_batches = math.ceil((num_batches - START) / (outer_batch * BATCH_SIZE))
 
-    
     with tqdm(total=len_batches) as pbar:
-        with concurrent.futures.ProcessPoolExecutor(max_workers=MAX_WORKERS) as executor:    
+        with concurrent.futures.ProcessPoolExecutor(
+            max_workers=MAX_WORKERS
+        ) as executor:
             for _ in range(outer_batches):
                 this_batch = [next(batches) for _ in range(outer_batch)]
                 futures = {executor.submit(run_, batch): batch for batch in this_batch}
@@ -50,5 +54,6 @@ def main():
                         return
                     pbar.update(1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
